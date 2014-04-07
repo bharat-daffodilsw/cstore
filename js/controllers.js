fff66babcbb42e5c59a42d0a6a3dbf9678fea6e7
bharat-daffodilsw <bharat.bhushan@daffodilsw.com>
2014-04-07 14:57:19 +0530

    'controller'

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,42 @@
-'use strict';
 var BAAS_SERVER = "/rest";
 var ASK = "531829f47754938f0ecfd3c7";
 var OSK = "531972e05fccddeb550a04a3";
 
 // Declare app level module which depends on filters, and services
-var cstore = angular.module('cstore', ['ngRoute','$appstrap.services']);
+var cstore = angular.module('cstore', ['ngRoute', '$appstrap.services']);
+cstore.config(
+    function ($routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider.
+            when('/test', {
+//            templateUrl:'../',
+//            controller:'mainCtrl'
+        }).otherwise(
+//            {"redirectTo":"/login.html"}
+        );
+    });
+
+cstore.controller('mainCtrl', function ($scope, $appService, $location) {
+    $scope.currentUser = {"data":""};
+    $scope.currentUser["data"]=$appService.getSession();
+    if(!$scope.currentUser["data"]){
+        alert();
+        window.location.href("http://127.0.0.1:5400/login.html");
+    }
+    $scope.auth = function () {
+        var currentSession = $scope.getSession();
+        if (currentSession) {
+//            window.location.href = "/#/home";
+        }
+    }
 
 
+    $scope.getClass = function (path) {
+        if ($location.path().substr(0, path.length) == path) {
+            return "active"
+        } else {
+            return ""
+        }
+    }
+});
 
--- js/cstore-strap.js
+++ js/cstore-strap.js
@@ -2,50 +2,6 @@ var BAAS_SERVER;
 BAAS_SERVER = "/rest";
 var appStrapDirectives = angular.module('$appstrap.directives', []);
 var appStrapServices = angular.module('$appstrap.services', []);
-var ASK = "5301e64492f51ed71842a5d1";
-var OSK = "530459b8aed74b22457bad37";
-'use strict';
-appStrapDirectives.directive('appCountrySelect', ['$compile', function ($compile) {
-    return {
-        restrict:"E",
-        replace:true,
-        scope:true,
-        template:'<select id="countryselect" name="country" ng-model="row.country" ng-change="getStates(row.country)">' +
-            '<option ng-repeat="country in country_options" value="{{country._id}}" >{{country.name}}</option>' +
-            '</select>',
-        compile:function () {
-            return {
-                pre:function ($scope) {
-                }
-            }
-        }
-    }
-}]);
-
-appStrapDirectives.directive('appStateSelect', ['$compile', function ($compile) {
-    return {
-        restrict:"E",
-        replace:true,
-        scope:true,
-        template:'<table>' +
-            '<tr><td>{{transbutton.country}}</td><td><app-country-select></app-country-select></td></tr>' +
-            '<tr><td>{{transbutton.state}}</td><td>' +
-            '<select name="state" ng-model="row.state" ng-change="getCities(row.state)">' +
-            '<option ng-repeat="state in state_options" value="{{state._id}}">{{state.name}}</option></select></td></tr>' +
-            '<tr><td>{{transbutton.city}}</td><td><select name="city" ng-model="row.city">' +
-            '<option ng-repeat="city in city_options" value="{{city._id}}">{{city.name}}</option></select></td>' +
-            '</tr>' +
-            '</table>',
-        compile:function () {
-            return {
-                pre:function ($scope) {
-
-                }
-            }
-        }
-    }
-}]);
-
 /**
  * App Services
  */
@@ -55,7 +11,6 @@ appStrapServices.factory('$appService', [
     '$timeout',
     function ($rootScope, $http, $timeout) {
         var $appService = {
-
         };
         $appService.login = function (username, password, callback) {
             var params = {};
@@ -70,155 +25,6 @@ appStrapServices.factory('$appService', [
             });
         };
 
-        $appService.logOut = function (callBack) {
-            this.getDataFromService("/backend/task_manager/logout", {}, "Logout...", callBack);
-        }
-
-        $appService.getUser = function (state, userprofileid, sortField, sortOrder, pageNumber, rowCount, callBack) {
-            var that = this;
-            var childCountthis = this;
-            var usk;
-            if (typeof(Storage) !== "undefined") {
-                //usk = localStorage.usk;
-                usk = $appService.checkCookie();
-            }
-
-            var hostedMode = false;
-            var serviceURL = "/data";
-            var CU_query = {"table":"users__baas", "filter":{"_id":"{_CurrentUserId}"}, "columns":["_id", "username", "picture", "emailid", "firstname", "lastname", "status", "isadmin", {"expression":"socialinfo", "columns":["userid"]}]};
-            var serviceCallBack;
-            if (hostedMode) {
-                if (userprofileid == 'currentuser') {
-                    var user = {"response":{"_id":"521c9b34bfbe4e7854000005", "username":"PP", "firstname":"Stan", "lastname":"Gursky", "designation":"CEO", "organization":"Marketech Inc.", "picture":[
-                        {"key":"522050840b75e9eb3800000d", "name":"user_profile.png"}
-                    ], "row":{"_id":"521c9b34bfbe4e7854000005", "username":"Stangursky", "firstname":"Stan", "lastname":"Gursky", "picture":[
-                        {"key":"522050840b75e9eb3800000d", "name":"user_profile.png"}
-                    ], "emailid":"abc@gmail.com", "phoneno":123456789, "organization":"UserOrg1", "designation":"Senior Associate IT", "membershipdate":"31/07/2012", "renewaldate":"1/08/2013"}, "login":true}, "status":"ok", "code":200};
-                    callBack(user.response);
-                    return;
-                }
-            } else {
-                if (userprofileid == 'newuser' || userprofileid == 'loggedinuser') {
-
-                    serviceCallBack = function (userInfo) {
-                        if (userInfo.forgotpasswordcode)
-                            delete userInfo.forgotpasswordcode;
-                        userInfo.row = {};
-                        // *done The “Member since” field should populate automatically when you create an account. But you need to still be able to manually override it too.
-                        var today = new Date();
-                        var dd = today.getDate();
-                        var mm = today.getMonth() + 1; //January is 0!
-                        var yyyy = today.getFullYear();
-                        if (dd < 10) {
-                            dd = '0' + dd
-                        }
-                        if (mm < 10) {
-                            mm = '0' + mm
-                        }
-                        today = mm + '/' + dd + '/' + yyyy;
-                        userInfo.row.membershipdate = today;
-                        userInfo.row.watchlistprivacy = true;
-                        callBack(userInfo);
-
-                    }
-                }
-                else if (userprofileid == 'currentuser') {
-
-                    serviceCallBack = function (userInfo) {
-                        if (userInfo.forgotpasswordcode)
-                            delete userInfo.forgotpasswordcode;
-                        userInfo.row = angular.copy(userInfo);
-                        callBack(userInfo);
-
-                    }
-                }
-                else if (userprofileid == 'adminuser') {
-                    serviceCallBack = function (userInfo) {
-                        if (userInfo.forgotpasswordcode)
-                            delete userInfo.forgotpasswordcode;
-                        var userDataServiceURL = "/data";
-                        var query = {"table":"users__baas", "columns":["_id", "username", "picture", "firstname", "lastname", "emailid", "status", "isadmin"], "$count":1};
-                        if (sortField) {
-                            var sortexpression = {};
-                            sortexpression[sortField] = (sortOrder == '') ? 'asc' : sortOrder;
-                            query = {"table":"users__baas", "orders":[sortexpression], "columns":["_id", "username", "picture", "emailid", "firstname", "lastname", "status", "isadmin"], "$count":1};
-                        }
-                        var limitexpression = {}
-                        if (pageNumber) {
-                            var rowIndex = (pageNumber - 1) * rowCount;
-                            query["max_rows"] = rowCount;
-                            query["cursor"] = rowIndex;
-                        }
-                        if ($("#searchbox_input").val()) {
-                            var searchbox_input = $("#searchbox_input").val().trim()
-                            if (searchbox_input.length > 0) {
-                                query["filter"] = {"$or":[
-                                    {"emailid":{"$regex":"(" + searchbox_input + ")", "$options":"-i"}},
-                                    {"firstname":{"$regex":"(" + searchbox_input + ")", "$options":"-i"}},
-                                    {"lastname":{"$regex":"(" + searchbox_input + ")", "$options":"-i"}}
-                                ]};
-                            }
-                        }
-                        var queryParams = {query:JSON.stringify(query), "ask":ASK, "usk":usk, "osk":OSK};
-                        that.getDataFromJQuery(BAAS_SERVER + userDataServiceURL, queryParams, "GET", "JSON", "Loading...", function (petSocialUserData) {
-                            var petSocialUserDataData = petSocialUserData.data;
-                            userInfo.childCount = petSocialUserData.$count;
-                            var count = petSocialUserDataData ? petSocialUserDataData.length : 0;
-
-                            for (var i = 0; i < count; i++) {
-                                var r = petSocialUserDataData[i];
-                                if (r['renewaldate'] && typeof(r['renewaldate']) !== 'undefined') {
-                                    r['renewaldate'] = r['renewaldate'];
-                                }
-                            }
-
-                            var metadata = {"type":"table", "table":"domains__frontend", "columns":[
-                                {"_id":"52032a47a23b2cf815000002", "type":"string", "expression":"firstname", "funName":"userSort", "sortable":true, "replicate":true, "primary":true, "columns":[], "label":"First Name", "width":112, "visibility":"Both", cellTemplate:"<a href=\"/edituser?userid={{row._id}}\" style='margin-right: 10%;text-decoration:none;'>{{row.firstname}}</a>", "$$hashKey":"07F"},
-                                {"_id":"52032a47a23b2cf815000002", "type":"string", "expression":"lastname", "funName":"userSort", "sortable":true, "replicate":true, "primary":true, "columns":[], "label":"Last Name", "width":112, "visibility":"Both", cellTemplate:"<a href=\"/edituser?userid={{row._id}}\" style='margin-right: 10%;text-decoration:none;'>{{row.lastname}}</a>", "$$hashKey":"07F"},
-                                {"_id":"520b689c4577828418000001", "type":"string", "expression":"emailid", "funName":"userSort", "sortable":true, "label":"Email", "width":196, "visibility":"Both"},
-                                {"_id":"520b689c4577828418000001", "type":"string", "expression":"phoneno", "funName":"userSort", "sortable":true, "label":"Phone", "width":115, "visibility":"Both"},
-                                {"_id":"520e0b9b72058ac80b000001", "type":"date", "expression":"renewaldate", "funName":"userSort", "sortable":true, "label":"Renewal for date", "width":138, "visibility":"Both"},
-                                {"_id":"520b689c4577828418000001", "expression":"actions", "label":"Actions", "width":260, "visibility":"Both", cellTemplate:"<span class='anchor-span' ng-show=row.status ng-click=\"userBlock(row,'status',false)\" style='width: 95px;display: block;float: left;'>Block user</span><span class='anchor-span' ng-hide=row.status ng-click=\"userBlock(row,'status',true)\" style='width: 95px;display: block;float: left;'>Unblock user</span><a href=\"/edituser?userid={{row._id}}\" style='margin-right: 10%;'>Edit user</a>  <span class='anchor-span' ng-click=\"userBlock(row,'userdelete',true)\" style='margin-right: 10%;' ng-hide=row.userdelete>Delete user</span><span class='anchor-span' ng-show=row.userdelete ng-click=\"userBlock(row,'userdelete',false)\" style='margin-right: 10%;'>Undelete</span>"}
-                            ]}
-
-                            var view = {data:mtmUserData, metadata:metadata};
-                            userInfo.view = view;
-                            callBack(userInfo);
-
-                        }, function (jqxhr, error) {
-                            callBack(error);
-                        });
-                    }
-
-                }
-                else {
-
-                    serviceCallBack = function (userInfo) {
-                        if (userInfo.forgotpasswordcode)
-                            delete userInfo.forgotpasswordcode;
-                        var pUserId = userprofileid;
-                        var userDataServiceURL = "/data";
-                        var query = {"table":"users__baas", "filter":{_id:pUserId}, "columns":["_id", "username", "emailid", "picture", "firstname", "lastname", "status", "isadmin"]};
-                        var queryParams = {query:JSON.stringify(query), "ask":ASK, "usk":usk, "osk":OSK};
-                        that.getDataFromJQuery(BAAS_SERVER + userDataServiceURL, queryParams, "GET", "JSON", "Loading...", function (mtmUserData) {
-
-                            userInfo.row = mtmUserData.data[0];
-                            callBack(userInfo);
-
-                        }, function (jqxhr, error) {
-                            callBack(error);
-                        });
-                    }
-                }
-
-            }
-            var parameterArr = {query:JSON.stringify(CU_query), "ask":ASK, "usk":usk, "osk":OSK};
-            this.getDataFromJQuery(BAAS_SERVER + serviceURL, parameterArr, "GET", "JSON", "Loading...", function (callBackData) {
-                serviceCallBack(callBackData.data[0]);
-            }, function (jqxhr, error) {
-                callBack(error);
-            });
-        }
 
         $appService.save = function (data, ask, osk, usk, callBack) {
             if (!ask) {
@@ -226,13 +32,12 @@ appStrapServices.factory('$appService', [
             }
             // return;
             var params;
-            if(usk){
+            if (usk) {
 
                 params = {"updates":JSON.stringify(data), "ask":ask, "osk":osk, "usk":usk};
             }
-            else
-            {
-                 params = {"updates":JSON.stringify(data), "ask":ask, "osk":osk};
+            else {
+                params = {"updates":JSON.stringify(data), "ask":ask, "osk":osk};
 
             }
             var that = this;
@@ -242,40 +47,6 @@ appStrapServices.factory('$appService', [
                 callBack(callBackData);
             });
         }
-
-        $appService.getDataFromService = function (pUrl, pData, pBusyMessage, pCallBack) {
-            var payLoad;
-            if (pData == null) {
-                pData = {};
-            }
-            pData.osk = this.osk;
-            pData.ask = this.ask;
-            var payLoad = $.param(pData);
-            var config = {
-                headers:{"Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"}
-            }
-            if (pBusyMessage) {
-                $rootScope.busymessage = pBusyMessage;
-                $rootScope.showbusymessage = true;
-                if (!$rootScope.$$phase) {
-                    $rootScope.$apply();
-                }
-            }
-            $http.post(pUrl, payLoad, config).success(
-                function (data) {
-                    $rootScope.showbusymessage = false;
-                    pCallBack(data);
-
-                }).error(function (data) {
-                    //alert("Error in Fetching service:[" + pUrl + "], Data[" + data + "]");
-                    $rootScope.showbusymessage = false
-                    if (!$rootScope.$$phase) {
-                        $rootScope.$apply();
-                    }
-                    pCallBack(data);
-                });
-        }
-
 
         $appService.delete_cookie = function (name) {
             document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
@@ -305,6 +76,7 @@ appStrapServices.factory('$appService', [
             }
             return c_value;
         }
+
         $appService.getDataFromJQuery = function (url, requestBody, callType, dataType, callback, errcallback) {
             $.support.cors = true;
 
@@ -345,6 +117,28 @@ appStrapServices.factory('$appService', [
                 dataType:dataType,
                 async:true
             });
+        }
+        $appService.getSession = function () {
+            var currentSession = {};
+            if (!$appService.getCookie("usk")) {
+                return null;
+            }
+            currentSession["usk"] = $appService.getCookie("usk");
+            currentSession["roleid"] = $appService.getCookie("roleid");
+            currentSession["userid"] = $appService.getCookie("userid");
+            currentSession["firstname"] = $appService.getCookie("firstname");
+            if ($appService.getCookie("storeid")) {
+                currentSession["storeid"] = $appService.getCookie("storeid");
+            }
+            return currentSession;
+        }
+        $appService.deleteAllCookie = function () {
+            $appService.delete_cookie("usk");
+            $appService.delete_cookie("role");
+            $appService.delete_cookie("userid");
+            $appService.delete_cookie("firstname");
+            $appService.delete_cookie("storeid");
+            $appService.delete_cookie("storename");
         }
         return $appService;
     }
